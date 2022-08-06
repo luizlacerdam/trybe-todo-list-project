5190adc385b5d1cfdb8ed983b2426dcaeab8a54a
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-06 13:10:07 -0300

    requisitos-5-e-6

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const LISTA_ORDENADA = document.getElementById('lista-tarefas');
+const INPUT = document.getElementById('texto-tarefa');
+const BUTTON = document.getElementById('criar-tarefa');
+
+function criarTarefa() {
+  BUTTON.addEventListener('click', function () {
+    let novaLi = document.createElement('li');
+    novaLi.innerText = INPUT.value;
+    LISTA_ORDENADA.appendChild(novaLi);
+    INPUT.value = ''
+  });
+}criarTarefa();
