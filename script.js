183ea8135674f93d9514611a80a1f1ebb8c3a7a0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-07 10:11:08 -0300

    requisitos-11

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const LISTA_ORDENADA = document.getElementById('lista-tarefas');
 const INPUT = document.getElementById('texto-tarefa');
 const BUTTON = document.getElementById('criar-tarefa');
 const ITENS = document.getElementsByTagName('li');
-const BTN_CLEAR = document.getElementById('apaga-tudo');
+const BTN_CLEAR_ALL = document.getElementById('apaga-tudo');
+const BTN_CLEAR_DONE = document.getElementById('remover-finalizados');
 
 function criarTarefa() {
   BUTTON.addEventListener('click', function () {
@@ -39,11 +40,22 @@ function completed() {
 }
 completed();
 
-function clear() {
-  BTN_CLEAR.addEventListener('click', function () {
+function clearAll() {
+  BTN_CLEAR_ALL.addEventListener('click', function () {
     while (LISTA_ORDENADA.firstChild) {
       LISTA_ORDENADA.removeChild(LISTA_ORDENADA.firstChild)
     }
   });
 }
-clear();
+clearAll();
+
+function clearDone() {
+  BTN_CLEAR_DONE.addEventListener('click', function () {
+    for (let i = ITENS.length - 1; i >= 0; i -= 1) {
+      if (ITENS[i].classList.contains('completed')) {
+        LISTA_ORDENADA.removeChild(ITENS[i]);
+      }
+    }
+  });
+}
+clearDone();
